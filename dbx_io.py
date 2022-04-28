8b6835a250e4764fb07f7385d270a76e44ee85f4
Garrett Warren <garrettwarren@Garretts-MacBook-Pro.local>
2022-04-27 23:23:49 -0400

    Use refresh token and app key for dropbox oauth

--- dbx_io.py
+++ dbx_io.py
@@ -3,21 +3,23 @@ import os
 import io
 import sys
 
-dbx = dropbox.Dropbox(os.environ.get('DROPBOX_ACCESS_TOKEN'))
-
+REFRESH_TOKEN=os.environ.get('DROPBOX_REFRESH_TOKEN')
+APP_KEY=os.environ.get("DROPBOX_APP_KEY")
 
 def read():
-    _, res = dbx.files_download("/roster.txt")
-    with io.BytesIO(res.content) as stream:
-        txt = stream.read().decode()
-        print(txt)
-        sys.stdout.flush()
+    with dropbox.Dropbox(oauth2_refresh_token=REFRESH_TOKEN, app_key=APP_KEY) as dbx:
+        _, res = dbx.files_download("/roster.txt")
+        with io.BytesIO(res.content) as stream:
+            txt = stream.read().decode()
+            print(txt)
+            sys.stdout.flush()
 
 
 def write(txt):
-    with io.BytesIO(txt.encode()) as stream:
-        stream.seek(0)
-        dbx.files_upload(stream.read(), "/roster.txt", mode=dropbox.files.WriteMode.overwrite)
+    with dropbox.Dropbox(oauth2_refresh_token=REFRESH_TOKEN, app_key=APP_KEY) as dbx:
+        with io.BytesIO(txt.encode()) as stream:
+            stream.seek(0)
+            dbx.files_upload(stream.read(), "/roster.txt", mode=dropbox.files.WriteMode.overwrite)
 
 
 
